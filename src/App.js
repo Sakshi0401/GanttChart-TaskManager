a91528ff672c09d2002be34213a8b14f1dab715a
Sakshi Joshi <joshisakshi0401@gmail.com>
2021-04-11 11:55:58 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Gantt from "./Components/Gantt";
+import "./App.css";
+import { Navbar, Card} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body ">
+      <div>
+        {/*NavBar starts here */}
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand>
+            <img
+              alt=""
+              src="https://img.icons8.com/cute-clipart/2x/task.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{'   '}
+            Task Manager
+          </Navbar.Brand>
+        </Navbar>
+        {/*NavBar ends here */}
+      </div>
+      <Card className="Card">
+        {/*Gantt Chart*/}
+        <Gantt></Gantt>
+      </Card>
     </div>
   );
 }
